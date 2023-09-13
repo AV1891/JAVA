474eb1cdb746f27c59d3b8fa27ad08c6736ff222
AV1891 <144378472+AV1891@users.noreply.github.com>
2023-09-13 14:00:03 -0400

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,14 @@
+//Program to add Two numbers
+class Main {
+  public static void main(String[] args) {
+      int a=10;
+      int y; //Declaration
+        y=5;//Initialization
+       System.out.println(a*y);
+    System.out.println(a/y);
+    System.out.println(a+y);
+    System.out.println(a-y);
+    
+
+  }
+}
