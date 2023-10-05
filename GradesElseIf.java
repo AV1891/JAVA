c6e67050aa144eff58407c81fc4aae91ebbf78a9
AV1891 <144378472+AV1891@users.noreply.github.com>
2023-10-05 13:11:04 -0400

    Create GradesElseIf.java

---
+++ GradesElseIf.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+   int x=67;
+    if(x>90)
+      System.out.println("The mark is A");
+     else if(x>80)
+      System.out.println("The mark is B");
+      else if(x>70)
+        System.out.println("The mark is C");
+    else if(x<70)
+        System.out.println("The mark is D");
+
+
+      
+       
+  }
+}
