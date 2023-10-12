15f4d52c42c6bec44be9d8676bea05a340cbfb99
AV1891 <144378472+AV1891@users.noreply.github.com>
2023-10-12 13:58:47 -0400

    Create Assigmnment1Part1C.java

---
+++ Assigmnment1Part1C.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      System.out.println("What is 9x9");
+      int Ans1 = scanner.nextInt();
+      
+      if (Ans1==81) {
+
+        System.out.println("Correct");
+      }
+      else{
+        System.out.println("Incorrect");
+      }
+      
+       System.out.println("What is 12x6");
+      int Ans2 = scanner.nextInt();
+      
+      if (Ans2==72) {
+
+        System.out.println("Correct");
+      }
+      else{
+        System.out.println("Incorrect");
+      }
+      
+      System.out.println("What is 22x3");
+      int Ans3 = scanner.nextInt();
+      
+      if (Ans3==66) {
+
+        System.out.println("Correct");
+      }
+      else{
+        System.out.println("Incorrect");
+      }
+      
+    
+    }
+      }
+      
+      
