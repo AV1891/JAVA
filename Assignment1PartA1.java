8c18a3955ea97b3f318795eb4f2c67bb85baa152
AV1891 <144378472+AV1891@users.noreply.github.com>
2023-10-12 13:41:10 -0400

    Update Assignment1PartA1.java

--- Assignment1PartA1.java
+++ Assignment1PartA1.java
@@ -7,9 +7,9 @@ class Main {
         String Name2 = scanner.nextLine();
 
         if (Name2.equals(Name1)) {
-            System.out.println("Hello, " + Name2 + "! You have the same name as programmer.");
+            System.out.println("Hello, " + Name2 + " You have the same name as programmer.");
         } else {
-            System.out.println("Hello, " + Name2 + "! Your name is different from programmer.");
+            System.out.println("Hello, " + Name2 + " Your name is different from programmer.");
         }
 
         scanner.close();
