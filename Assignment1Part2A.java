0c7bc204119b743e45f116fdc7177abe48e636ca
AV1891 <144378472+AV1891@users.noreply.github.com>
2023-10-14 22:36:39 -0400

    Update Assignment1Part2A.java

--- Assignment1Part2A.java
+++ Assignment1Part2A.java
@@ -5,16 +5,14 @@ import java.util.Scanner;
             Scanner scanner = new Scanner(System.in);
             System.out.print("Where are you from? ");
             String place1 = scanner.nextLine();
-
             if (place1.equals("India")) {
-              
                 System.out.println("India is known for its diverse culture and delicious food.");
             } 
             else if (place1.equals("Canada")) {
                 System.out.println("Canada is famous for Newtons Grove.");
             } 
             else if (place1.equals("America")) {
-                System.out.println("The United States is one of the biggest countries in the world in size and peoplw.");
+                System.out.println("The United States is one of the biggest countries in the world in size and people.");
             } 
             else {
                 System.out.println("I don't know anything about that place.");
