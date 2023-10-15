22674aca4770bb6c40b434476a79b7c5238112e1
AV1891 <144378472+AV1891@users.noreply.github.com>
2023-10-14 22:37:57 -0400

    Update Assigmnment1Part1C.java

--- Assigmnment1Part1C.java
+++ Assigmnment1Part1C.java
@@ -4,39 +4,29 @@ class Main {
       Scanner scanner = new Scanner(System.in);
       System.out.println("What is 9x9");
       int Ans1 = scanner.nextInt();
-      
       if (Ans1==81) {
-
         System.out.println("Correct");
       }
       else{
         System.out.println("Incorrect");
       }
-      
        System.out.println("What is 12x6");
       int Ans2 = scanner.nextInt();
-      
       if (Ans2==72) {
-
         System.out.println("Correct");
       }
       else{
         System.out.println("Incorrect");
       }
-      
       System.out.println("What is 22x3");
       int Ans3 = scanner.nextInt();
-      
       if (Ans3==66) {
-
         System.out.println("Correct");
       }
       else{
         System.out.println("Incorrect");
-      }
-      
     
-    }
-      }
-      
+ }
+ }
+}
       
