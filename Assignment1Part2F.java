b90844f5e2ea9416824355ecdcceaca526057b79
AV1891 <144378472+AV1891@users.noreply.github.com>
2023-10-15 22:05:53 -0400

    Create Assignment1Part2F.java

---
+++ Assignment1Part2F.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Choose your preferred language. e = English, f = French, p = Punjabi");
+        char lang = scanner.next().charAt(0);
+
+        if (lang == 'e') {
+            System.out.println("How are you?");
+        } else if (lang == 'f') {
+            System.out.println("Comment ça va?");
+        } else if (lang == 'p') {
+            System.out.println("ਤੁਸੀਂ ਕਿਵੇਂ ਹੋ? ");
+        } else {
+            System.out.println("Sorry, we don't have a greeting in your preferred language.");
+        }
+        scanner.close();
+    }
+}
